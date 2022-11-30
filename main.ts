56626fd433087cc691314b6ad6c42a9cd1cebcf3
g30egzo <119530750+g30egzo@users.noreply.github.com>
2022-11-30 19:38:58 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,32 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(kovetkezo)
+    kovetkezo += 1
+})
+input.onButtonPressed(Button.B, function () {
+    if (kovetkezo == _2 + _1) {
+        basic.pause(2000)
+        basic.showString(" UGYES VAGY!")
+    }
+})
+let kovetkezo = 0
+let _1 = 0
+let _2 = 0
+_2 = randint(0, 10)
+_1 = randint(0, 10)
+basic.showNumber(_1)
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
+basic.pause(1000)
+basic.showNumber(_2)
+basic.clearScreen()
+basic.forever(function () {
+    if (kovetkezo == 21) {
+        kovetkezo = 0
+    }
+})
